1ffaa95ca475c1ee5972aea16a45fe2950805fa8
Li <yli2935@uwo.ca>
2023-11-29 12:14:09 -0500

    finsh nav bar and import asset

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+/*
+ * @Description: temp
+ */
 import { useState } from 'react'
 import { BrowserRouter } from "react-router-dom";
-
+// import { Footer } from "./components/Footer";
+import Navbar from "./components/Navbar";
 import Routes from './routes';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes />
+      {/* <Footer /> */}
     </BrowserRouter>
   )
 }
